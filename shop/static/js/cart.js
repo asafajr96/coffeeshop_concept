b79ce7defef2a0ab798961fdca1c7e20c2780ad2
Asafa Makan'a <asafajr96@gmail.com>
2020-10-20 17:00:45 +0200

    small

--- shop/static/js/cart.js
+++ shop/static/js/cart.js
@@ -4,6 +4,14 @@ for (i = 0; i < updateBtns.length; i++){
     updateBtns[i].addEventListener('click', function(){
         var productId = this.dataset.product
         var action = this.dataset.action
-        console.log('productId:', productId, 'Action:', action)
+        console.Log('productId:', productId, 'Action:', action)
+
+        console.Log('USER:', user)
+        if (user == 'AnonymousUser'){
+            console.log('User is not authenticated')
+        }
+        else{
+            console.log('[User is authenticated, sending data...')
+        }
     })
 }
